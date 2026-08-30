4a709a258dbbac8be4cc8906054d83a9dc030acb
dipankrr <dipankar.laptop@gmail.com>
2026-08-31 03:23:10 +0530

    style(theme): change shadcn theme preset to --preset b3ktoFdFw

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,13 +1,13 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { TrpcProvider } from "@/lib/trpc/provider";
-import { Outfit, Inter } from "next/font/google";
+import { Outfit, Inter, Space_Grotesk } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner"
 
 import { ThemeProvider } from "@/components/theme-provider"
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const spaceGrotesk = Space_Grotesk({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "mskool",
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={cn("font-sans", inter.variable)} suppressHydrationWarning>
+    <html lang="en" className={cn("font-sans", spaceGrotesk.variable)} suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute="class"
